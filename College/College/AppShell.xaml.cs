74bbd71b6604969da4b6c02385db4ea822cf2b82
Ujjwal Kc <kcujwal98@gmail.com>
2024-12-21 19:23:40 +0545

    Don't what i did

--- College/College/AppShell.xaml.cs
+++ College/College/AppShell.xaml.cs
@@ -5,6 +5,7 @@
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("TechStd",typeof(TechStdPage));
         }
     }
 }
---
+++ College/College/TechStd.xaml.cs
@@ -0,0 +1,14 @@
+namespace College;
+
+public partial class TechStdPage : ContentPage
+{
+	public TechStdPage()
+	{
+		InitializeComponent();
+	}
+	private void OnClicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
